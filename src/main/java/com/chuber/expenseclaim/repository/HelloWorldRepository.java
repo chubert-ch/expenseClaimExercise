2334d9946142f62c1f95b1a500323de8fd97a154
Christopher Hubert <chubert.ch@googlemail.com>
2026-03-30 22:21:56 +0100

    Excising Hello World

--- src/main/java/com/chuber/expenseclaim/repository/HelloWorldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chuber.expenseclaim.repository;
-
-import com.chuber.expenseclaim.entity.HelloWorld;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloWorldRepository extends JpaRepository<HelloWorld, Long> {
-}
--- src/main/resources/db/migration/V1__create_hello_world.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE hello_world (
-    id   BIGSERIAL PRIMARY KEY,
-    message VARCHAR(255) NOT NULL
-);
-
-INSERT INTO hello_world (message) VALUES ('Hello from Postgres!');
